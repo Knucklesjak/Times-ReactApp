40813faf61ca3998b2c22c8d14080566eb8476c7
Jason Kunkel <jasonakunkel@outlook.com>
2018-03-28 21:33:04 -0700

    added code to components

---
+++ client/src/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>
+  <div className="page-header text-center ">
+   <h1>
+      New York Times Scraper<br/>
+      <small>Search for articles, save and comment the ones you like!!</small>
+   </h1>
+   
+</div>;
+
+export default Header;
