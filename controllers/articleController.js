c06a445bff897b99d81f6ee50ba2d658bd33817b
Jason Kunkel <jasonakunkel@outlook.com>
2018-03-28 22:55:22 -0700

    started controllers file on server side

---
+++ controllers/articleController.js
@@ -0,0 +1,18 @@
+const db = require("../models");
+
+// Defining methods for the articlesController
+module.exports = {
+  findAll: function(req, res) {
+    db.Article
+      .find(req.query)
+      .populate("comment")
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Article
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
